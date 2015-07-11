fc6f28caa87163612712b9ed97eef36c61cfe2be
Tobias Ranft <develop@ranft.biz>
2015-07-11 10:58:19 +0200

    Refined dates.

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 var App = angular.module('dashboardApp', []);
-App.controller('LogfileCtrl', ["$scope", "$http", LogfileCtrl]);
 
+App.controller('LogfileCtrl', ["$scope", "$http", LogfileCtrl]);
 function LogfileCtrl($scope, $http) {
   console.log("LogfileCtrl is in charge");
 
@@ -18,26 +18,57 @@ function LogfileCtrl($scope, $http) {
   refresh();
 }
 
-App.controller('RssfeedCtrl', ["$scope", "RssfeedService", RssfeedCtrl]);
 
+App.controller('RssfeedCtrl', ["$scope", "RssfeedService", RssfeedCtrl]);
 function RssfeedCtrl($scope, RssfeedService) {
   console.log("RssfeedCtrl is in charge");
   var feedUrl = 'http://www.vg.no/rss/nyfront.php?frontId=1';
-  var loadFeed = function(url) {
+  function loadFeed(url) {
     RssfeedService.parseFeed(url).then(function (res) {
       console.log("I got the rssfeed data I requested");
-      $scope.feeds = res.data.responseData.feed.entries;
-      $scope.feeds.sort(function (a,b){
-        a = new Date(a.publishedDate);
-        b = new Date(b.publishedDate);
-        if (a < b)
-          return 1;
-        if (a > b)
-          return -1;
-        return 0;
-      });
+      var entries = res.data.responseData.feed.entries;
+      entries = addDateObjects(entries);
+      entries = refineDateString(entries);
+      entries.sort(feedCompareDateDesc);
+      $scope.feeds = entries;
     });
   }
+
+  function addDateObjects(list) {
+    var numberDate;
+    for (var index=0; index < list.length; index++) {
+        list[index].dateObj = new Date(list[index].publishedDate);
+    }
+    return list;
+  }
+
+  function refineDateString(list) {
+    var monthNames = [
+      "Januar", "Februar", "Mars",
+      "April", "May", "Juni",
+      "Juli", "August", "September",
+      "Oktober", "November", "December"];
+    var date;
+
+    for (var index=0; index < list.length; index++) {
+      date = list[index].dateObj;
+      list[index].publishedDate = date.getDate() + ' ';
+      list[index].publishedDate += monthNames[date.getMonth()-1] + ' ';
+      list[index].publishedDate += date.getFullYear() + ' ';
+      list[index].publishedDate += date.getHours() + ':' + date.getMinutes();
+    }
+
+    return list;
+  }
+
+  function feedCompareDateDesc(a,b) {
+    if (a.dateObj < b.dateObj)
+      return 1;
+    if (a.dateObj > b.dateObj)
+      return -1;
+    return 0;
+  };
+
   loadFeed(feedUrl);
 }
 
@@ -48,37 +79,53 @@ function JsonfeedCtrl($scope, $http) {
 
   var loadFeed = function(url) {
     $http.get(url).success(function (res) {
-      var monthNames = new Array("Januar", "Februar", "Mars",
-        "April", "May", "Juni", "Juli", "August", "September",
-        "Oktober", "November", "December");
       console.log("I got the jsonfeed data I requested");
-
-      var month;
-      var numberDate;
-      for (var index=0; index < res.length; index++) {
-        for (month=1; month <= 12; month++) {
-          numberDate = res[index].date.replace(' '+monthNames[month-1]+' ', '.'+month+'.');
-          res[index].dateObj = new Date(numberDate+' '+ res[index].time);
-        }
-      }
+      res = addDateObjects(res);
+      res = refineDateString(res);
+      res.sort(feedCompareDateDesc);
       $scope.feeds = res;
-      $scope.feeds.sort(feedCompareDateDesc)
     });
   };
 
-  var feedCompareDateDesc = function(a,b) {
+  function addDateObjects(list) {
+    var monthNames = [
+      "Januar", "Februar", "Mars",
+      "April", "May", "Juni",
+      "Juli", "August", "September",
+      "Oktober", "November", "December"];
+    var month;
+    var numberDate;
+    for (var index=0; index < list.length; index++) {
+      for (month=1; month <= 12; month++) {
+        numberDate = list[index].date.replace(
+          ' ' + monthNames[month-1] + ' ',
+          '.' + month + '.'
+        );
+        list[index].dateObj = new Date(numberDate + ' ' + list[index].time);
+      }
+    }
+    return list;
+  }
+
+  function refineDateString(list) {
+    for (var index=0; index < list.length; index++) {
+      list[index].publishedDate = list[index].date + ' ' + list[index].time;
+    }
+    return list;
+  }
+
+  function feedCompareDateDesc(a,b) {
     if (a.dateObj < b.dateObj)
       return 1;
     if (a.dateObj > b.dateObj)
       return -1;
     return 0;
-  }
+  };
   loadFeed(feedUrl);
 }
 
 
 App.factory('RssfeedService', ['$http', RssfeedService]);
-
 function RssfeedService($http) {
   return {
     parseFeed: function (url) {
